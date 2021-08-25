49f4430f954c19c39ad5c69138fabad27a0f357c
MiareJ <psdsd@naver.com>
2021-08-25 19:05:33 +0900

    게시판 목록, 입력, 상세 / 헤더 드롭다운 추가, 수정중

--- js/header.js
+++ js/header.js
@@ -3,6 +3,36 @@ const menu = document.querySelectorAll('.navbar_menu');
 const searchIcon = document.querySelector('.search_icon');
 
 toggleBtn.addEventListener("click", () => {
-    menu.forEach(elem => elem.classList.toggle('hidden'));
-    searchIcon.classList.toggle('hidden')
+    menu.forEach(elem => elem.classList.toggle('active'));
+    searchIcon.classList.toggle('active')
+});
+
+const subMenu = document.querySelectorAll('.sub_menu');
+
+menu[0].addEventListener("mouseenter", () => {
+    subMenu[0].classList.toggle('active');
+});
+menu[0].addEventListener("mouseleave", () => {
+    subMenu[0].classList.toggle('active');
+});
+
+menu[1].addEventListener("mouseenter", () => {
+    subMenu[1].classList.toggle('active');
+});
+menu[1].addEventListener("mouseleave", () => {
+    subMenu[1].classList.toggle('active');
+});
+
+menu[2].addEventListener("mouseenter", () => {
+    subMenu[2].classList.toggle('active');
+});
+menu[2].addEventListener("mouseleave", () => {
+    subMenu[2].classList.toggle('active');
+});
+
+menu[3].addEventListener("mouseenter", () => {
+    subMenu[3].classList.toggle('active');
+});
+menu[3].addEventListener("mouseleave", () => {
+    subMenu[3].classList.toggle('active');
 });
