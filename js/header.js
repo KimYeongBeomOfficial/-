705414e07770a327fc956b4c9e262151ab215042
MiareJ <psdsd@naver.com>
2021-08-26 20:53:59 +0900

    header.js 코드 줄임

--- js/header.js
+++ js/header.js
@@ -9,37 +9,11 @@ toggleBtn.addEventListener("click", () => {
 
 const subMenu = document.querySelectorAll('.sub_menu');
 
-menu[0].addEventListener("mouseenter", () => {
-    subMenu[0].classList.toggle('active');
-});
-menu[0].addEventListener("mouseleave", () => {
-    subMenu[0].classList.toggle('active');
-});
-
-menu[1].addEventListener("mouseenter", () => {
-    subMenu[1].classList.toggle('active');
-});
-menu[1].addEventListener("mouseleave", () => {
-    subMenu[1].classList.toggle('active');
-});
-
-menu[2].addEventListener("mouseenter", () => {
-    subMenu[2].classList.toggle('active');
-});
-menu[2].addEventListener("mouseleave", () => {
-    subMenu[2].classList.toggle('active');
-});
-
-menu[3].addEventListener("mouseenter", () => {
-    subMenu[3].classList.toggle('active');
-});
-menu[3].addEventListener("mouseleave", () => {
-    subMenu[3].classList.toggle('active');
-});
-
-menu[4].addEventListener("mouseenter", () => {
-    subMenu[4].classList.toggle('active');
-});
-menu[4].addEventListener("mouseleave", () => {
-    subMenu[4].classList.toggle('active');
-});
+for (let i = 0; i < menu.length; i++) {
+    menu[i].addEventListener("mouseenter", () => {
+        subMenu[i].classList.add('active');
+    });
+    menu[i].addEventListener("mouseleave", () => {
+        subMenu[i].classList.remove('active');
+    });
+}
